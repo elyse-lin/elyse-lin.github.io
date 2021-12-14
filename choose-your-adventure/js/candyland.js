4376d54380e8fcc4cbd5f49a9bba2d6a9162693d
elyse-lin <elin@eastsideprep.org>
2021-12-14 14:09:48 -0800

    message 1

--- choose-your-adventure/js/candyland.js
+++ choose-your-adventure/js/candyland.js
@@ -1,1 +1,12 @@
 alert("Hello traveler!")
+let gameContinue = "y";
+while (gameContinue === "y") {
+    //printing welcome message
+    let welcomeMsg = "Welcome to Candyland. In front of you lies an entrance to two different realms. One realm is evil candyland, and the other is winter wonderland candyland. Good luck!";
+    alert(welcomeMsg);
+    //get player input
+    let playerChoice = Number(prompt("Which realm do you choose (1 or 2)?"));
+    //generate random number for realm
+    //random number 1 or 2
+
+}
