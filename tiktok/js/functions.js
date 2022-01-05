2cc91b2e44e481b7e428952ea4a6dd9586e1053a
elyse-lin <elin@eastsideprep.org>
2022-01-05 14:55:45 -0800

    tiktok

---
+++ tiktok/js/functions.js
@@ -0,0 +1,29 @@
+console.log("hello world!")
+
+//simple function, no parameters
+function hello() {
+    console.log("Hi Rami!");
+}
+
+function hello2(name) {
+    console.log("Hi " + name);
+}
+
+function annoyingGreet(name) {
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+    console.log("Hi " + name);
+}
+
+
+hello();
+hello2("Akilesh");
+hello2("Jack");
+hello2("Elyse");
+annoyingGreet("Elyse");
