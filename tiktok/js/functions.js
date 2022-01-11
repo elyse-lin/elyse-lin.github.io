272c1bf34e073a68144ae53de5b260cd86d00f30
elyse-lin <elin@eastsideprep.org>
2022-01-11 14:06:26 -0800

    tiktok

--- tiktok/js/functions.js
+++ tiktok/js/functions.js
@@ -19,8 +19,40 @@ function annoyingGreet(name) {
     console.log("Hi " + name);
     console.log("Hi " + name);
     console.log("Hi " + name);
+
+    /*another way to write it:
+    let counter = 0;
+   
+    while (counter < 10) {
+        hello2(name);
+        counter++;
+    */
+
 }
 
+function sumOfNumbers(p1, p2) {
+    let result = p1 + p2;
+    return result;
+}
+
+let answer = sumOfNumbers(1, 2);
+console.log(answer);
+
+function square(p1) {
+    let result = p1 * p1;
+    return result;
+}
+
+let answer2 = square(2);
+console.log(answer2);
+
+function sumOfSquares(num1, num2) {
+    let result = sumOfNumbers(square(num1), square(num2));
+    return result;
+}
+
+let answer3 = sumOfSquares(5, 6);
+console.log(answer3);
 
 hello();
 hello2("Akilesh");
