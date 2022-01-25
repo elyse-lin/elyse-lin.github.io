a248e95423f0311f5a477407d61f77c274e35cdc
elyse-lin <elin@eastsideprep.org>
2022-01-25 14:41:33 -0800

    buttons working

---
+++ virtual-pet/js/pet.js
@@ -0,0 +1,15 @@
+let water = 0;
+let sun = 0;
+let fertilization = 0;
+
+function waterPlant() {
+    console.log("Watering plant...");
+}
+
+function sunPlant() {
+    console.log("Giving plant sun...")
+}
+
+function fertilePlant() {
+    console.log("Fertilizing plant...")
+}
