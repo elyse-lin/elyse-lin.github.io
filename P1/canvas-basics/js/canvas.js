d9e6e496156ca78ecf8abc39e182a19a3080c759
elyse-lin <elin@eastsideprep.org>
2022-02-02 09:33:45 -0800

    bullseye

--- P1/canvas-basics/js/canvas.js
+++ P1/canvas-basics/js/canvas.js
@@ -20,7 +20,24 @@ let myCanvas = document.getElementById("my-canvas");
 //create canvas object
 let ctx = myCanvas.getContext("2d");
 
+ctx.fillStyle = "CornflowerBlue";
 ctx.fillRect(0, 0, 200, 100);
-ctx.fillStyle = "RebeccaPurple";
 
 //draw lines
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(0, 100);
+ctx.lineTo(200, 0);
+ctx.stroke();
+
+//draw circle
+for (let counter = 1; counter <= 10; counter += 2) {
+    ctx.beginPath();
+    ctx.arc(100, 50, counter * 5, 0, 2 * Math.PI);
+    ctx.strokeStyle = "Red";
+    ctx.stroke();
+}
