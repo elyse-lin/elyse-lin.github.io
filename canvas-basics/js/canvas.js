c6a5fe8adacc98d30f62abd745c4dea29873c3a8
elyse-lin <elin@eastsideprep.org>
2022-02-02 08:55:44 -0800

    experimenting

--- canvas-basics/js/canvas.js
+++ canvas-basics/js/canvas.js
@@ -1,1 +1,16 @@
-console.log("test");
+//console.log("test");
+
+//let's do for loops
+//count 1-10
+for (let counter = 1; counter <= 10; counter++) {
+    console.log(counter);
+}
+
+//count 10-1
+for (let counter1 = 10; counter1 >= 1; counter1--) {
+    console.log(counter1);
+}
+//count 1-10 only odd numbers
+for (let counter2 = 1; counter2 <= 10; counter2 += 2) {
+    console.log(counter2);
+}
