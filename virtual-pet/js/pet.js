69f91b69132864ba765e34e863e75974da7704ca
elyse-lin <elin@eastsideprep.org>
2022-02-01 16:41:53 -0800

    new conditions and stop function

--- virtual-pet/js/pet.js
+++ virtual-pet/js/pet.js
@@ -1,6 +1,8 @@
 let water = 5;
 let sun = 5;
 let fertilization = 2;
+let isAlive = true;
+
 
 function waterPlant() {
     console.log("Watering plant...");
@@ -55,45 +57,54 @@ function refreshUI() {
     //update pet image and paragraph
     let petImg = document.getElementById("pet-img");
     let petParagraph = document.getElementById("pet-condition");
-    if (water <= 3 && sun <= 3 && fertilization <= 1) {
+    if (water <= 3 && sun <= 3 && fertilization <= 1 && isAlive) {
+        petImg.src = "images/deadcactus.jpg"
+        petParagraph.innerHTML = "i'm dead"
+        alert("sorry, your plant died. please refresh the page to start again!");
+        isAlive = false;
+
+    } else if (water < 1 && isAlive) {
+        petImg.src = "images/deadcactus.jpg"
+        petParagraph.innerHTML = "i'm dead"
+        alert("sorry, your plant died. please refresh the page to start again!");
+        isAlive = false;
+    } else if (sun < 1 && isAlive) {
+        petImg.src = "images/deadcactus.jpg"
+        petParagraph.innerHTML = "i'm dead"
+        alert("sorry, your plant died. please refresh the page to start again!");
+        isAlive = false;
+    } else if (fertilization === 0 && isAlive) {
         petImg.src = "images/deadcactus.jpg"
         petParagraph.innerHTML = "i'm dead"
         alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (water < 1) {
-        petImg.src = "images/deadcactus.jpg"
-        petParagraph.innerHTML = "i'm dead"
-        alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (sun < 1) {
-        petImg.src = "images/deadcactus.jpg"
-        petParagraph.innerHTML = "i'm dead"
-        alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (fertilization <= 1) {
-        petImg.src = "images/deadcactus.jpg"
-        petParagraph.innerHTML = "i'm dead"
-        alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (water <= 3 && sun <= 3 && fertilization <= 5) {
-        petImg.src = "images/deadcactus.jpg"
-        petParagraph.innerHTML = "i'm dead"
-        alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (water <= 10 && sun <= 3 && fertilization <= 2) {
-        petImg.src = "images/deadcactus.jpg"
-        petParagraph.innerHTML = "i'm dead"
-        alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (water <= 6 && sun <= 3 && fertilization <= 5) {
-        petImg.src = "images/deadcactus.jpg"
-        petParagraph.innerHTML = "i'm dead"
-        alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (water <= 6 && sun <= 6 && fertilization <= 3) {
+        isAlive = false;
+    } else if (water <= 3 && sun <= 3 && fertilization <= 5 && isAlive) {
+        petImg.src = "images/unhealthycactus.jpg"
+        petParagraph.innerHTML = "i'm unhealthy, quick save me!"
+    } else if (water <= 7 && sun <= 7 && fertilization <= 2 && isAlive) {
+        petImg.src = "images/unhealthycactus.jpg"
+        petParagraph.innerHTML = "i'm unhealthy, quick save me!"
+    } else if (water <= 7 && sun <= 3 && fertilization <= 4 && isAlive) {
+        petImg.src = "images/unhealthycactus.jpg"
+        petParagraph.innerHTML = "i'm unhealthy, quick save me!"
+    } else if (water <= 3 && sun <= 7 && fertilization <= 4 && isAlive) {
+        petImg.src = "images/unhealthycactus.jpg"
+        petParagraph.innerHTML = "i'm unhealthy, quick save me!"
+    } else if (water <= 8 && sun <= 8 && fertilization <= 3 && isAlive) {
         petImg.src = "images/healthycactus.jpg"
         petParagraph.innerHTML = "i'm healthy, good job!"
-    } else if (water <= 3 && sun <= 10 && fertilization <= 3) {
+    } else if (water <= 6 && sun <= 3 && fertilization <= 5 && isAlive) {
+        petImg.src = "images/unhealthycactus.jpg"
+        petParagraph.innerHTML = "i'm unhealthy, quick save me!"
+    } else if (water <= 4 && sun <= 10 && fertilization <= 2 && isAlive) {
         petImg.src = "images/dry cactus.jpg"
         petParagraph.innerHTML = "i'm dry, quick save me!"
-    } else if (water <= 10 && sun <= 3 && fertilization <= 3) {
+    } else if (water <= 10 && sun <= 2 && fertilization <= 2 && isAlive) {
         petImg.src = "images/deadcactus.jpg"
         petParagraph.innerHTML = "i'm dead"
         alert("sorry, your plant died. please refresh the page to start again!")
-    } else if (water >= 7 && sun >= 7 && fertilization >= 4) {
+        isAlive = false;
+    } else if (water > 7 && sun > 7 && fertilization > 3 && isAlive) {
         petImg.src = "images/blossomingcactus.jpg"
         petParagraph.innerHTML = "i blossomed. yay"
     }
@@ -104,12 +115,21 @@ function refreshUI() {
 setInterval(function decreasePet() {
     if (water > 0) {
         water -= .25;
+        if (isAlive === false) {
+            return;
+        }
     }
     if (sun > 0) {
         sun -= .25;
+        if (isAlive === false) {
+            return;
+        }
     }
     if (fertilization > 0) {
         fertilization -= .25;
+        if (isAlive === false) {
+            return;
+        }
     }
 
     refreshUI();
