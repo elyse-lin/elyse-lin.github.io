d439a4047acef6e3d2cfe4302a34c6b5e4ea1741
elyse-lin <elin@eastsideprep.org>
2022-02-08 09:28:54 -0800

    moving rectangle

--- P1/canvas-animation/js/animation.js
+++ P1/canvas-animation/js/animation.js
@@ -2,6 +2,71 @@
 let myCanvas = document.getElementById("my-canvas");
 let ctx = myCanvas.getContext("2d");
 
+//get paragraph items
+let keydownOutput = document.getElementById("keydown-output");
+let keyupOutput = document.getElementById("keyup-output");
+
+//player position and movement
+let playerX = 250;
+let playerY = 250;
+let playerSpeed = 2;
+let playerXDir = 0;
+let playerYDir = 0;
+
+function drawPlayer() {
+    ctx.fillRect(playerX, playerY, 100, 20);
+
+}
+
+function movePlayer() {
+    playerX += playerSpeed * playerXDir;
+    playerY += playerSpeed * playerYDir;
+}
+
+
+function refreshPlayer() {
+    ctx.clearRect(0, 0, 500, 500);
+    movePlayer();
+    drawPlayer();
+}
+//when key is pressed
+function keyPressed(event) {
+    // get the actual key pressed
+    let key = event.keyCode;
+    keydownOutput.innerHTML = "key down code: " + key;
+
+    //move player
+    if (key === 37) {
+        playerXDir = -1;
+    } else if (key === 39) {
+        playerXDir = 1;
+    }
+    if (key === 38) {
+        playerYDir = -1;
+    } else if (key === 40) {
+        playerYDir = 1;
+
+    }
+}
+//when key is released
+function keyReleased(event) {
+    // get the actual key pressed
+    let key = event.keyCode;
+    keyupOutput.innerHTML = "key up code: " + key;
+
+    if (key === 37) {
+        playerXDir = 0;
+    } else if (key === 39) {
+        playerXDir = 0;
+    }
+    if (key === 38) {
+        playerYDir = 0;
+    } else if (key === 40) {
+        playerYDir = 0;
+
+    }
+}
+
 //object position
 let xPosition = 0;
 let yPosition = 0;
@@ -53,5 +118,7 @@ function rectBounce() {
     }
 }
 
-setInterval(rectBounce, 10);
+//setInterval(rectBounce, 10);
+setInterval(refreshPlayer, 30);
+
 //#3 ball bouncing horizontally
