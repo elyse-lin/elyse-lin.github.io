fff9edc136cc23d5df7b52c4b4263cdb2772f7b5
elyse-lin <elin@eastsideprep.org>
2022-02-14 08:46:56 -0800

    updated width constant var

--- P1/canvas-animation/js/animation.js
+++ P1/canvas-animation/js/animation.js
@@ -12,13 +12,15 @@ let playerY = 250;
 let playerSpeed = 2;
 let playerXDir = 0;
 let playerYDir = 0;
+const PADDLE_WIDTH = 100;
+const PADDLE_HEIGHT = 20;
 
 //ball position and movement
 let ballX = 50;
 let ballY = 100;
 let ballXDir = 2.5;
 let ballYDir = 2;
-let ballRadius = 15;
+const BALL_RADIUS = 15;
 
 function drawPlayer() {
     ctx.fillRect(playerX, playerY, 100, 20);
@@ -31,8 +33,8 @@ function movePlayer() {
     //edge check
     if (playerX < 0) {
         playerX = 0;
-    } else if (playerX > 500 - 101) {
-        playerX = 500 - 101;
+    } else if (playerX > 500 - PADDLE_WIDTH) {
+        playerX = 500 - PADDLE_WIDTH;
     }
 }
 
@@ -49,7 +51,7 @@ function refreshPlayer() {
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+    ctx.arc(ballX, ballY, BALL_RADIUS, 0, 2 * Math.PI);
     ctx.fill();
 }
 
@@ -60,10 +62,10 @@ function moveBall() {
 
 function checkBallCollision() {
     //check vertical wall
-    if ((ballY > 500 - ballRadius) || (ballY < 0 + ballRadius)) {
+    if ((ballY > 500 - BALL_RADIUS) || (ballY < 0 + BALL_RADIUS)) {
         ballYDir = ballYDir * -1;
     }
-    if ((ballX > 500 - ballRadius) || (ballX < 0 + ballRadius)) {
+    if ((ballX > 500 - BALL_RADIUS) || (ballX < 0 + BALL_RADIUS)) {
         ballXDir = ballXDir * -1;
     }
 }
