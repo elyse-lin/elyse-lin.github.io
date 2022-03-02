2a69e1e31780927ba8d8c03dc3f6fde7d6f000a0
elyse-lin <elin@eastsideprep.org>
2022-03-02 13:37:44 -0800

    changes with collision problems

--- P1/final-project/js/animation.js
+++ P1/final-project/js/animation.js
@@ -9,7 +9,7 @@ let wave = document.getElementById("wave");
 
 
 let playerX = 0;
-let playerY = 250;
+let playerY = 125;
 let playerSpeed = 2;
 let playerXDir = 0;
 let playerYDir = 0;
@@ -19,15 +19,18 @@ const IMG_WIDTH = 70;
 const IMG_HEIGHT = 50;
 const GEM_WIDTH = 40;
 const GEM_HEIGHT = 40;
+let points = 0;
+let newgem1;
 
 let keydownOutput = document.getElementById("keydown-output");
 let keyupOutput = document.getElementById("keyup-output");
+let playerPoints = document.getElementById("points");
 
 function drawLine() {
     ctx.lineWidth = 7;
     ctx.beginPath();
-    ctx.moveTo(0, 167);
-    ctx.lineTo(1000, 167);
+    ctx.moveTo(0, 50);
+    ctx.lineTo(1000, 50);
     ctx.stroke();
 
 }
@@ -35,17 +38,17 @@ function drawLine() {
 function drawLine2() {
     ctx.lineWidth = 7;
     ctx.beginPath();
-    ctx.moveTo(0, 333);
-    ctx.lineTo(1000, 333);
+    ctx.moveTo(0, 250);
+    ctx.lineTo(1000, 250);
     ctx.stroke();
 
 }
 
 function drawImage() {
-    ctx.drawImage(gem1, 200, 250, GEM_WIDTH, GEM_HEIGHT);
-    ctx.drawImage(gem2, 450, 280, GEM_WIDTH, GEM_HEIGHT);
+    newgem1 = ctx.drawImage(gem1, 250, 50, GEM_WIDTH, GEM_HEIGHT);
+    ctx.drawImage(gem2, 450, 100, GEM_WIDTH, GEM_HEIGHT);
     ctx.drawImage(monster, 600, 180, GEM_WIDTH, GEM_HEIGHT);
-    ctx.drawImage(gem3, 800, 250, GEM_WIDTH, GEM_HEIGHT);
+    ctx.drawImage(gem3, 800, 150, GEM_WIDTH, GEM_HEIGHT);
     //ctx.drawImage(wave, 750, 100, IMG_WIDTH, IMG_HEIGHT);
     //ctx.drawImage(wave, 250, 400, IMG_WIDTH, IMG_HEIGHT);
     //ctx.drawImage(wave, 100, 50, IMG_WIDTH, IMG_HEIGHT);
@@ -65,6 +68,27 @@ function movePlayer() {
         playerX = 0;
     } else if (playerX > 1000 - SWIMMER_WIDTH) {
         playerX = 1000 - SWIMMER_WIDTH;
+    }
+}
+
+function checkSwimmerCollision() {
+    //check vertical wall
+    if (playerY > 250 - SWIMMER_HEIGHT) {
+        playerY = 250 - SWIMMER_HEIGHT;
+    } else if (playerY < 50) {
+        playerY = 50;
+    }
+    //check horizontal wall
+    if (playerX > 1000 - SWIMMER_WIDTH) {
+        playerX = 1000 - SWIMMER_WIDTH
+    } else if (playerX < 0) {
+        playerX = 0;
+    }
+
+    // gem and monster collision
+    if (newgem1 + GEM_WIDTH >= playerX && newgem1 <= playerX + SWIMMER_WIDTH && newgem1 + GEM_HEIGHT >= playerY && playerY <= newgem1 + GEM_HEIGHT) {
+        newgem1.style.display = 'none';
+        points += 1;
     }
 }
 
@@ -103,13 +127,19 @@ function keyPressed(event) {
     }
 }
 
+function pointsDisplay() {
+    playerPoints.innerHTML = "Points: " + points;
+}
+
 function refreshPlayer() {
-    ctx.clearRect(0, 0, 1000, 500);
+    ctx.clearRect(0, 0, 1000, 300);
     drawImage();
     drawLine();
     drawLine2();
     movePlayer();
     drawPlayer();
+    checkSwimmerCollision();
+    pointsDisplay();
 }
 
 setInterval(refreshPlayer, 10);
